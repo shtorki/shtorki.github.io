67d90711e6b0ed4de29ba84b309216c3613bb833
dyhere <dy.here@yandex.com>
2016-10-10 19:18:43 +0300

    +

---
+++ src/js/dy.js
@@ -0,0 +1,10 @@
+// Other
+var showPhones =  function () {
+    $("#modalContacts").modal('show');
+};
+$("#showPhones").on('click',showPhones);
+$("#showPhones2").on('click',showPhones);
+$(".showPhones").on('click', showPhones);
+
+// TODO: Get items from dGalery
+

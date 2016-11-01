f1364e3539d41b422fe4029cbb90b69a1a70921a
dyhere <dy.here@yandex.com>
2016-11-01 13:44:25 +0200

    +

--- src/js/dy.js
+++ src/js/dy.js
@@ -8,3 +8,25 @@ $(".showPhones").on('click', showPhones);
 
 // TODO: Get items from dGalery
 
+// ###################################
+// ######### Load Dictionary #########
+// ###################################
+var dic = "src/dics/ru.json";
+
+if(navigator.language != "ru") {
+    dic.replace("ru","eng");
+}
+
+var loadDic = function (jsonDic) {
+// Test
+//    console.log($("body").html().match(/%\w*%/g));
+//     $(".slider-main").html($(".slider-main").html());
+};
+
+$.ajax({
+    url: dic,
+    cache: true
+})
+    .done(function( data ) {
+        loadDic(data);
+    });

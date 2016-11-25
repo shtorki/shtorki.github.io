33803f8ab7d1e689689caf0554089237c4a5caf1
dyhere <dy.here@yandex.com>
2016-11-25 15:15:02 +0200

    update

--- src/js/dy.js
+++ src/js/dy.js
@@ -5,28 +5,3 @@ var showPhones =  function () {
 $("#showPhones").on('click',showPhones);
 $("#showPhones2").on('click',showPhones);
 $(".showPhones").on('click', showPhones);
-
-// TODO: Get items from dGalery
-
-// ###################################
-// ######### Load Dictionary #########
-// ###################################
-var dic = "src/dics/ru.json";
-
-if(navigator.language != "ru") {
-    dic.replace("ru","eng");
-}
-
-var loadDic = function (jsonDic) {
-// Test
-//    console.log($("body").html().match(/%\w*%/g));
-//     $(".slider-main").html($(".slider-main").html());
-};
-
-$.ajax({
-    url: dic,
-    cache: true
-})
-    .done(function( data ) {
-        loadDic(data);
-    });
